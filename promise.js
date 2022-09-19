249f64ddc9f4873ecde263f26c0526d748bab622
Diki Darussalam <dadsnaho@gmail.com>
2022-09-20 01:07:29 +0700

    mengerjakan todo pada promise.js

--- promise.js
+++ promise.js
@@ -1,8 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const gabungtheater = [...theaterIXX, ...theaterVGC]
+  let jumlahEmosi = 0;
+  gabungtheater.forEach((film) => {
+    if (film.hasil === emosi) {
+      jumlahEmosi++;
+    };
+  })
 
+ return jumlahEmosi;
+};
+
+console.log(promiseOutput('marah'))
+console.log(promiseOutput('tidak marah'))
 module.exports = {
   promiseOutput,
 };
